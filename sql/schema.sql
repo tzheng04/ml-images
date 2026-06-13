4845cf8cb81f8f0d388680ae5dea38b2ceb3ee12
Tony Zheng <tzheng12@terpmail.umd.edu>
2026-06-13 13:39:58 -0400

    added SQl schema

---
+++ sql/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE models (
+    id SERIAL PRIMARY KEY,
+    version_name TEXT NOT NULL,
+    model_path TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    notes TEXT
+);
+
+CREATE TABLE predictions (
+    id SERIAL PRIMARY KEY,
+    true_label TEXT,
+    predicted_label TEXT NOT NULL,
+    image_b64 TEXT,
+    was_correct BOOLEAN,
+    confidence FLOAT,
+    model_id INTEGER REFERENCES models(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE feedback (
+    id SERIAL PRIMARY KEY,
+    prediction_id INTEGER REFERENCES predictions(id),
+    corrected_label TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
