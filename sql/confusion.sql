8fa6da08182608287b61a92ff1dbd1db8060aeff
Tony Zheng <tzheng12@umd.edu>
2026-06-19 13:36:46 -0400

    added confusion.sql

---
+++ sql/confusion.sql
@@ -0,0 +1,8 @@
+SELECT
+    true_label,
+    predicted_label,
+    COUNT(*) as num_instances
+FROM predictions
+WHERE NOT was_correct
+GROUP BY true_label, predicted_label
+ORDER BY num_instances DESC
