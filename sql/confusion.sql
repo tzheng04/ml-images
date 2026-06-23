f291659f4f0f59a04decd28661641a97c8fec340
Tony Zheng <tzheng12@umd.edu>
2026-06-23 11:20:19 -0400

    reorganized directory for dockerization

--- sql/confusion.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    true_label,
-    predicted_label,
-    COUNT(*) as num_instances
-FROM predictions
-WHERE NOT was_correct
-GROUP BY true_label, predicted_label
-ORDER BY num_instances DESC, true_label ASC
-LIMIT %s OFFSET %s;
