121aaffd2a82b452ff4774ba4eb3967f386fcb28
Tony Zheng <tzheng12@terpmail.umd.edu>
2026-06-23 09:31:12 -0400

    dockerization

--- src/app.py
+++ src/app.py
@@ -66,4 +66,4 @@ def query():
 if __name__ == "__main__":
     # port = int(os.environ.get("PORT", 10000))
     # app.run(host="0.0.0.0", port=port)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
